6651096c7f1fc31657097dd2e97f0ecaa12dabab
无可无不可 <2752962035@qq.com>
2020-02-21 01:42:33 +0800

    update TestGitBranches.java 文件
    新增一行测试代码。。。

--- src/main/java/com/atguigu/java/TestGitBranches.java
+++ src/main/java/com/atguigu/java/TestGitBranches.java
@@ -4,6 +4,7 @@ public class TestGitBranches {
 
 	public static void main(String[] args) {
 		System.out.println("我是master分支，我用于开发基础模块...");
+		System.out.println("我想测试下如何将当前分支代码往master分支上面合并...");
 	}
 	
 }
