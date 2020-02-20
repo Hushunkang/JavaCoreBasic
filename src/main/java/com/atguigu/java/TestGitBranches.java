4efb01c56b518edd6fb972ffa579522978309aee
Hushunkang <2752962035@qq.com>
2020-02-21 02:06:14 +0800

    测试代码

--- src/main/java/com/atguigu/java/TestGitBranches.java
+++ src/main/java/com/atguigu/java/TestGitBranches.java
@@ -6,6 +6,9 @@ public class TestGitBranches {
 		System.out.println("我是master分支，我用于开发基础模块...");
 		System.out.println("我想测试下如何将当前分支代码往master分支上面合并...");
 		System.out.println("我是hot_fix分支，我用于开发支付模块...");
+
+
+		System.out.println("俺是hsk_code分支...");
 	}
 	
 }
