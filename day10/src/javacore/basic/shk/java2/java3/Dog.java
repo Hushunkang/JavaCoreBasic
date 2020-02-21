75e32bb73d8746b6063041fcd0112b46cec196e5
Hushunkang <2752962035@qq.com>
2020-02-22 03:13:45 +0800

    Java 核心知识 基础部分 code && 案例 shk code

---
+++ day10/src/javacore/basic/shk/java2/java3/Dog.java
@@ -0,0 +1,5 @@
+package javacore.basic.shk.java2.java3;
+
+public class Dog {
+
+}
---
+++ day11/src/javacore/basic/shk/java/Creature.java
@@ -0,0 +1,9 @@
+package javacore.basic.shk.java;
+
+public class Creature {
+	
+	public void breath(){
+		System.out.println("呼吸");
+	}
+	
+}
